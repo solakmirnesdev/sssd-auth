f1b8a7b3a4593ad9e579b3c1642850f9cdb84726
Mirnes Solak <solakmirnes.dev@gmail.com>
2024-05-26 20:10:59 +0200

    adding copy file as a placeholder

--- config_default.copy.php
+++ config_default.copy.php
@@ -1,106 +1,21 @@
 <?php
 
-/**
- * Configuration file for the application.
- * Never delete nor update this file!
- *
- * This file contains configuration settings for the database and mail services.
- * Update the values with your specific configuration.
- *
- * @return array The configuration array.
- */
-return [
-    'db' => [
-        /**
-         * Database host.
-         *
-         * @var string
-         */
-        'host' => 'your_hosted_database_host',
+define('DB_HOST', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '!o1');
+define('DB_NAME', '');
+define('DB_PORT', '');
 
-        /**
-         * Database name.
-         *
-         * @var string
-         */
-        'dbname' => 'your_database_name',
+define('TEXT_MESSAGE_API_KEY', '');
+define('TEXT_MESSAGE_SECRET', '');
 
-        /**
-         * Database username.
-         *
-         * @var string
-         */
-        'username' => 'your_database_username',
+define('SMTP_HOST', '');
+define('SMTP_USERNAME', '');
+define('SMTP_PASSWORD', '');
+define('SMTP_PORT', '');
+define('SMTP_ENCRYPTION', '');
 
-        /**
-         * Database password.
-         *
-         * @var string
-         */
-        'password' => 'your_database_password',
+define('HCAPTCHA_SERVER_SECRET', '');
+define('HCAPTCHA_SITE_KEY', '');
 
-        /**
-         * Database charset.
-         *
-         * @var string
-         */
-        'charset' => 'utf8mb4',
-    ],
-    'mail' => [
-        /**
-         * Mail server host.
-         *
-         * @var string
-         */
-        'host' => 'sandbox.smtp.mailtrap.io',
-
-        /**
-         * Enable SMTP authentication.
-         *
-         * @var bool
-         */
-        'smtp_auth' => true,
-
-        /**
-         * SMTP username.
-         *
-         * @var string
-         */
-        'username' => 'your_mailtrap_username',
-
-        /**
-         * SMTP password.
-         *
-         * @var string
-         */
-        'password' => 'your_mailtrap_password',
-
-        /**
-         * SMTP encryption type.
-         *
-         * @var string
-         */
-        'smtp_secure' => PHPMailer::ENCRYPTION_STARTTLS,
-
-        /**
-         * SMTP server port.
-         *
-         * @var int
-         */
-        'port' => 2525,
-
-        /**
-         * The sender email address.
-         *
-         * @var string
-         */
-        'from_email' => 'no-reply@example.com',
-
-        /**
-         * The sender name.
-         *
-         * @var string
-         */
-        'from_name' => 'Mailer',
-    ]
-];
+define('JWT_SECRET', '');
--- config_default.php
+++ config_default.php
@@ -1,28 +1,21 @@
 <?php
 
-// SSSD project config default, has to be called config_default.php
-
-// Database connection
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Supermar!o1');
 define('DB_NAME', 'sssd_auth');
-define('DB_PORT', '3306'); // Default MySQL port, change if different
+define('DB_PORT', '3306');
 
-// Text messages
 define('TEXT_MESSAGE_API_KEY', 'your_text_message_api_key');
-define('TEXT_MESSAGE_SECRET', ''); // Will be empty for Infobip
+define('TEXT_MESSAGE_SECRET', '');
 
-// Email
 define('SMTP_HOST', 'sandbox.smtp.mailtrap.io');
 define('SMTP_USERNAME', 'cb215d12ba5592');
 define('SMTP_PASSWORD', '2d778308ff5ab9');
-define('SMTP_PORT', '2525'); // Mailtrap port
-define('SMTP_ENCRYPTION', 'tls'); // tls or ssl
+define('SMTP_PORT', '2525');
+define('SMTP_ENCRYPTION', 'tls');
 
-// hcaptcha
 define('HCAPTCHA_SERVER_SECRET', 'your_hcaptcha_server_secret');
 define('HCAPTCHA_SITE_KEY', 'your_hcaptcha_site_key');
 
-// JWT Secret
 define('JWT_SECRET', 'somesecret');
