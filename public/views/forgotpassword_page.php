c5d15db1a8a0f6e6c5034722bee6fbfce9e3546b
Mirnes Solak <solakmirnes.dev@gmail.com>
2024-05-27 01:18:04 +0200

    Forgot password UI

--- public/views/forgotpassword_page.php
+++ public/views/forgotpassword_page.php
@@ -6,14 +6,15 @@
 	<script src="/views/scripts.js"></script>
 </head>
 <body>
-<h1>Forgot Password</h1>
 <form id="forgotPasswordForm" onsubmit="handleSubmit(event, 'forgotPasswordForm', '/api/forgot-password')">
+<h1>Forgot Password</h1>
 	<label for="email">Email:</label>
 	<input type="email" id="email" name="email" required><br>
 
 	<button type="submit">Reset Password</button>
+	<div id="message"></div>
+	<a href="/login">Login</a>
+	<a href="/register">Register</a>
 </form>
-<div id="message"></div>
-<a href="/login">Login</a> | <a href="/register">Register</a>
 </body>
 </html>
